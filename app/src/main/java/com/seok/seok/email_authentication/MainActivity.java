bd107902ea62d70f234e22dd20a4778d00d6c185
msnodeve <msnodeve@gmail.com>
2019-02-03 20:46:04 +0900

    Create dialog

--- app/src/main/java/com/seok/seok/email_authentication/MainActivity.java
+++ app/src/main/java/com/seok/seok/email_authentication/MainActivity.java
@@ -1,13 +1,41 @@
 package com.seok.seok.email_authentication;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText authEmail;
+    Button authBtn;
+
+    LayoutInflater dialog; //LayoutInflater
+    View dialogLayout; //layout View
+    Dialog authDialog; //dialog Object
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        authEmail=(EditText)findViewById(R.id.authEmail);
+        authBtn=(Button)findViewById(R.id.authButton);
+        authBtn.setOnClickListener((View.OnClickListener) this);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.authButton :
+                dialog = LayoutInflater.from(this);
+                dialogLayout = dialog.inflate(R.layout.auth_dialog, null); // LayoutInflater를 통해 XML에 정의된 Resource들을 View의 형태로 반환 시켜 줌
+                authDialog = new Dialog(this); //Dialog 객체 생성
+                authDialog.setContentView(dialogLayout); //Dialog에 inflate한 View를 탑재 하여줌
+                authDialog.setCanceledOnTouchOutside(false); //Dialog 바깥 부분을 선택해도 닫히지 않게 설정함.
+                authDialog.show(); //Dialog를 나타내어 준다.
+        }
     }
 }
